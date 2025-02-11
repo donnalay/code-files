9ddf1de9424a81ac343483256dfa7bf0685d2b00
Donna Lay <laydnna@gmail.com>
2025-02-11 07:56:55 -0500

    bar chart

--- chapter_03/3.3-Binding_data/start/js/main.js
+++ chapter_03/3.3-Binding_data/start/js/main.js
@@ -2,8 +2,7 @@
 const svg = d3
   .select(".responsive-svg-container")
   .append("svg")
-  .attr("viewBox", "0 0 1200 1600")
-  .style("border", "1px solid black");
+  .attr("viewBox", "0 0 600 700");
 
 // Load, format and measure the dataset
 d3.csv("../start/data/data.csv", (d) => {
@@ -34,17 +33,55 @@ d3.csv("../start/data/data.csv", (d) => {
 // Create the bar graph
 const createViz = (data) => {
   const barHeight = 20;
-  svg
+  const xScale = d3.scaleLinear().domain([0, 1078]).range([0, 450]);
+  const yScale = d3
+    .scaleBand()
+    .domain(data.map((d) => d.technology))
+    .range([0, 600])
+    .paddingInner(0.2);
+
+  const barAndLabel = svg
     .selectAll("rect")
     .data(data)
-    .join("rect")
+    .join("g")
+    .attr("transform", (d) => `translate(0, ${yScale(d.technology)})`);
+
+  barAndLabel
+    .append("rect")
     .attr("class", (d) => {
       return `bar bar-${d.technology}`;
     })
-    .attr("width", (d) => d.count)
-    .attr("height", barHeight)
-    .attr("x", 0)
-    .attr("y", (d, i) => (barHeight + 4) * i)
-    .attr("fill", "#bae0ff")
-    .attr("fill", (d) => (d.technology === "D3.js" ? "#b7eb8f" : "#bae0ff"));
+    .attr("width", (d) => xScale(d.count))
+    .attr("height", yScale.bandwidth())
+    .attr("x", 100)
+    .attr("y", 0)
+    .attr("fill", "#4096ff");
+  // .attr("fill", (d) => (d.technology === "D3.js" ? "#b7eb8f" : "#4096ff"));
+
+  barAndLabel
+    .append("text")
+    .text((d) => d.technology)
+    .attr("x", 96)
+    .attr("y", 12)
+    .attr("text-anchor", "end")
+    .style("font-family", "sans-serif")
+    .style("font-size", "10px")
+    .style("color", "#808080");
+
+  barAndLabel
+    .append("text")
+    .text((d) => d.count)
+    .attr("x", (d) => 100 + xScale(d.count) + 4)
+    .attr("y", 12)
+    .style("font-family", "sans-serif")
+    .style("font-size", "10px")
+    .style("color", "#808080");
+
+  svg
+    .append("line")
+    .attr("x1", 100)
+    .attr("y1", 0)
+    .attr("x2", 100)
+    .attr("y2", 600)
+    .attr("stroke", "#888888");
 };
